bcafb3142d91790e6d06162f5aec33633692a5ef
Revekka <revekka.gershovich@gmail.com>
2026-05-11 10:38:28 -0400

    waits to run weekly review automatically added but it has not worked yet

--- run_weekly_review.sh
+++ run_weekly_review.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# Only run on the first Friday of the month (days 1–7)
-if [ "$(date +%-d)" -gt 7 ]; then
-    exit 0
-fi
 # Wait up to 5 minutes for network before proceeding
 for i in $(seq 1 30); do
     if /usr/bin/curl -s --max-time 5 https://api.todoist.com > /dev/null 2>&1; then
@@ -12,6 +8,6 @@ for i in $(seq 1 30); do
 done
 cd /Users/revekkagershovich/Projects/Orgs/orgs
 /opt/anaconda3/envs/Revekka_first_environment/bin/jupyter nbconvert \
-    --to notebook --execute monthly_review.ipynb \
-    --output /tmp/monthly_review_last_run.ipynb \
-    >> /Users/revekkagershovich/Projects/Orgs/orgs/monthly_review_cron.log 2>&1
+    --to notebook --execute weekly_review.ipynb \
+    --output /tmp/weekly_review_last_run.ipynb \
+    >> /Users/revekkagershovich/Projects/Orgs/orgs/weekly_review_cron.log 2>&1
